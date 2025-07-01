6353fac97f485d7259dd82d58a4007e3691ecf88
Tarek592 <muhammadtarek5920@gmail.com>
2025-07-01 17:10:50 +0600

    project palish complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect } from 'react';
-import Slider from '../components/Slider';
-
-
-import FoodSupportSection from '../components/FoodSupportSection';
-import Service from '../components/Service';
-import FeaturedFoods from '../components/featuredFoods';
-import PromotionBanner from '../components/PromotionBanner';
-
-
+import React, { useEffect } from "react";
+import Slider from "../components/Slider";
+import FoodSupportSection from "../components/FoodSupportSection";
+import Service from "../components/Service";
+import PromotionBanner from "../components/PromotionBanner";
+import FeaturedFoods from "../components/FeaturedFoods";
 const Home = () => {
-     useEffect(() => {
-        document.title = "Home | FOOD KING";
-      }, []);
-    return (
-        <div>
-            <Slider></Slider>
-            <FeaturedFoods></FeaturedFoods>
-            <Service></Service>
-            <FoodSupportSection></FoodSupportSection>
-            <PromotionBanner></PromotionBanner>
-           
-        </div>
-    );
+  useEffect(() => {
+    document.title = "Home | FOOD KING";
+  }, []);
+  return (
+    <div>
+      <Slider></Slider>
+      <FeaturedFoods></FeaturedFoods>
+      <Service></Service>
+      <FoodSupportSection></FoodSupportSection>
+      <PromotionBanner></PromotionBanner>
+    </div>
+  );
 };
 
 export default Home;
